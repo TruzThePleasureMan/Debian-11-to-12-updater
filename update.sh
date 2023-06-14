232fafe40f453d2c44564a398ee988a9745a9dfd
TruzThePleasureMan <piotrantos90@gmail.com>
2023-06-14 18:27:51 +0200

    Full automation

--- update.sh
+++ update.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Check if sudo is installed
+if ! command -v sudo &> /dev/null; then
+  echo "sudo is not installed. Installing sudo..."
+  su -c "apt-get update -y && apt-get install -y sudo"
+fi
+
+# Set debconf selections
+echo '* libraries/restart-without-asking boolean true' | sudo debconf-set-selections
+
+
 # Update Debian 11
 sudo apt update -y 
 sudo apt upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confnew" -o Dpkg::Options::="--force-confmiss"
@@ -15,6 +25,9 @@ sudo sed -i 's/bullseye/bookworm/g' /etc/apt/sources.list
 
 # Update sources
 sudo apt update -y 
+
+# Configure openssh-server
+sudo dpkg-reconfigure -f noninteractive openssh-server
 
 # Start upgrade
 sudo apt full-upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confnew" -o Dpkg::Options::="--force-confmiss"
